ebdcfc014c09b49c48f0dd25c998805d7adf0a18
Pavel Martynov <25pashok@mail.ru>
2024-12-18 02:03:42 +0500

    Функция создания карточек и вывод шести карточек из массива на экран

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list')
+const cardTemplate = document.querySelector('#card-template').content
 
-// @todo: DOM узлы
+function createCard(data) {
+    const cardElement = cardTemplate.cloneNode(true)
+    const cardImage = cardElement.querySelector('.card__image')
+    const cardTitle = cardElement.querySelector('.card__title')
+    const deleteButton = cardElement.querySelector('.card__delete-button')
+    const likeButton = cardElement.querySelector('.card__like-button')
 
-// @todo: Функция создания карточки
+    cardTitle.textContent = data.name
+    cardImage.src = data.link
+    cardImage.alt = data.name
 
-// @todo: Функция удаления карточки
+    return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+    placesList.append(createCard(item))
+});
